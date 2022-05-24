0c9a0a70a2d6e809e378482162caccf23ca12988
Marçal Junior <marssaljr@gmail.com>
2022-05-23 23:08:40 -0300

    feat: create product and ensure product has a image

--- store/models.py
+++ store/models.py
@@ -3,5 +3,14 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
 	img = models.ImageField(upload_to='img')
+	#def __str__(self) -> str:
+		#return self.img.url
+
+class Product(models.Model):
+	name = models.CharField(max_length=200)
+	images = models.ManyToManyField(Image)
+	price = models.FloatField()
+	description = models.TextField()
+
 	def __str__(self) -> str:
-		return self.img.url
+		return self.name
