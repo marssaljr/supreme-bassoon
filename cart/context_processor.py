544e72c70b8800696ee14860414ac17779c59740
Marçal Junior <marssaljr@gmail.com>
2022-06-26 17:43:33 -0300

    feat: add context processor for amount bill

---
+++ cart/context_processor.py
@@ -0,0 +1,12 @@
+from .cart import Cart
+
+def cart_total_amount(request):
+		if request.user.is_authenticated:
+				cart=Cart(request)
+				total_bill=0.0
+				for k,v in request.session['cart'].items():
+						total_bill=total_bill+(float(v['price'])*v['quantity'])
+				return {'cart_total_amount':total_bill}
+		else:
+				return {'cart_total_amount':0}
+				
