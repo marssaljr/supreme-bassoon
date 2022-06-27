cdb143e0ca13cc4fb13d0a3dfb0f0ff9a6bf518c
Marçal Junior <marssaljr@gmail.com>
2022-06-27 18:06:20 -0300

    test: a lot of stuff that I forgot to commit

--- cart/tests/test_cart.py
+++ cart/tests/test_cart.py
@@ -1,8 +1,1 @@
 from django.test import TestCase
-
-# Create your tests here.
-class CartTestCase(TestCase):
-		def test_init(self):
-				self.client.get('/cart/add/1')
-				session=self.client.session
-#				self.assertEqual(session['cart'], )
