066523949daa0b0d0a38dfa3b9688553356dcea3
Marçal Junior <marssaljr@gmail.com>
2022-07-08 18:54:58 -0300

    fix: address input form length

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, default="", null=True, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, default="Cliente gente boa :)", blank=True)
-    location = models.CharField(max_length=30, default="Lugar nenhum", blank=True)
+    location = models.CharField(max_length=255, default="Lugar nenhum", blank=True)
     birth_date = models.DateField(null=True, blank=True, default=datetime.date.today)
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
 
