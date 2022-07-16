7398eff06a7266d1ad7050086069a872dbfa9c6f
Marçal Junior <marssaljr@gmail.com>
2022-07-16 20:56:56 -0300

    feat: implement stripe checkout button click

--- templates/static/js/main.js
+++ templates/static/js/main.js
@@ -1,1 +1,23 @@
-console.log('sanity check!')
+// get Stripe publishable key
+fetch("/config/")
+	.then((result) => { return result.json(); })
+	.then((data) => {
+		// initialize Stripe.js
+		const stripe = Stripe(data.publicKey);
+
+		// button click event handler
+		document.querySelector("#comprar").addEventListener("click", () => {
+			// Get Session ID
+			fetch("/create-checkout-session/")
+				.then((result) => { return  result.json(); })
+				.then((data) => {
+
+					// console.log(data);
+					// Redirect to Stripe Checkout
+					return stripe.redirectToCheckout({sessionId: data.sessionId})
+				})
+				.then((res) => {
+					console.log(res);
+				});
+		});
+	});
