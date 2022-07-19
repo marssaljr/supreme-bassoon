16f65e0d55206fa971fefd465a95276ac73bb6d0
Marçal Junior <marssaljr@gmail.com>
2022-07-19 15:37:42 -0300

    test: user profile returns a user instance

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,9 +1,24 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
-from user.models import *
+from django.test import TestCase, Client, RequestFactory
+
+from user.forms import RegisterForm, UpdateProfileForm
 import tempfile
 
 
 class UserModelsTestCase(TestCase):
     def setUp(self):
-        newProfile = Profile.objects.create()
+        self.jacob = {
+            "first_name": "jacob",
+            "last_name": "mcdonalds",
+            "username": "jacob",
+            "email": "jacob@gmail.com",
+            "password1": "top_secret123213",
+            "password2": "top_secret123213",
+        }
+        self.form = RegisterForm(data=self.jacob)
+        self.form.save()
+
+    def test_user_has_a_profile(self):
+        self.assertEquals(
+            self.form.instance.profile.__str__(), self.form.instance.username
+        )
